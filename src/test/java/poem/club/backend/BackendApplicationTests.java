c90a1cd947d663f94545016e9c67d801537d6462
dm002314473 <dm002314473@fer.hr>
2025-02-22 14:50:37 +0100

    Initial spring boot app

---
+++ src/test/java/poem/club/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package poem.club.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
